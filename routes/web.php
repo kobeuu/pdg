61d616306d8a5f16a40cfd4d7a0cf87365a87649
kobeuu <kobeuu@gmail.com>
2017-07-02 22:25:12 +0700

    tambah rute pendaftar

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', 'HomeController@index')->name('home')->middleware('auth');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registrants Routes...
+Route::get('registrants', 'RegistrantController@index')->name('registrants');
