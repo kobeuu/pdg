fcf1e51391fe58417a35d9be38c62942500ac9e6
kobeuu <kobeuu@gmail.com>
2017-07-02 22:26:10 +0700

    tambah menu, fix active class menu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -20,26 +20,29 @@
     <ul class="sidebar-menu">
       <li class="header">MAIN NAVIGATION</li>
       <!-- Optionally, you can add icons to the links -->
-      <li class="active"><a href="/"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-      <li><a href="/registrants"><i class="fa fa-link"></i> <span>Pendaftar</span></a></li>
-      <li><a href="/calculate"><i class="fa fa-calculator"></i> <span>Perhitungan</span></a></li>
+      <li {{{ (Request::is('/') ? 'class=active' : '') }}} ><a href="/"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+      <li {{{ (Request::is('registrants') ? 'class=active' : '') }}} ><a href="/registrants"><i class="fa fa-users"></i> <span>Semua Pendaftar</span></a></li>
       <li class="treeview">
-        <a href="/setting"><i class="fa fa-cog"></i> <span>Pengaturan</span>
+        <a href="/setting"><i class="fa fa-spinner"></i> <span>Proses Seleksi</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="#"><i class="fa fa-circle-o"></i>Parameter</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i>Kriteria</a></li>
           <li><a href="#"><i class="fa fa-circle-o"></i>Bobot</a></li>
         </ul>
       </li>
+      <li><a href="/calculate"><i class="fa fa-check"></i> <span>Penerima Manfaat</span></a></li>
+      <li {{{ (Request::is('report') ? 'class=active' : '') }}} ><a href="/report"><i class="fa fa-line-chart"></i> <span>Statistik</span></a></li>
+      <li {{{ (Request::is('settings') ? 'class=active' : '') }}} ><a href="/settings"><i class="fa fa-gear"></i> <span>Pengaturan</span></a></li>
+      <li {{{ (Request::is('documentations') ? 'class=active' : '') }}} ><a href="/documentations"><i class="fa fa-book"></i> <span>Dokumentasi</span></a></li>
       <li><a href="{{ route('logout') }}"
               onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
 
           <!-- hidden-xs hides the username on small devices so only the image appears. -->
-          <i class="fa fa-power-off"></i> <span>Keluar</span>
+          <i class="fa fa-power-off"></i> <span>Keluar Aplikasi</span>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
               {{ csrf_field() }}
           </form>
