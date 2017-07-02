6ad463654cf1ee888797e904f6730fa3e2769352
kobeuu <kobeuu@gmail.com>
2017-07-02 22:24:45 +0700

    tambah pendaftar

---
+++ app/Http/Controllers/RegistrantController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Registrant;
+use Illuminate\Http\Request;
+
+class RegistrantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $registrants = Registrant::all();
+        return view('registrants.index', compact($registrants));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Registrant  $registrant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Registrant $registrant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Registrant  $registrant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Registrant $registrant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Registrant  $registrant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Registrant $registrant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Registrant  $registrant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Registrant $registrant)
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_07_02_125821_create_registrants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('address');
+            $table->text('images');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrants');
+    }
+}
