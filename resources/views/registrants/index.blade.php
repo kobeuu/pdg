19b6e77fdfb7781b4913fec5034be585462d535b
kobeuu <kobeuu@gmail.com>
2017-07-02 23:19:14 +0700

    merancang daftar kolom pendaftar

--- resources/views/registrants/index.blade.php
+++ resources/views/registrants/index.blade.php
@@ -11,115 +11,47 @@
   <div class="col-xs-12">
     <div class="box">
       <div class="box-header">
-        <h3 class="box-title">Data Table With Full Features</h3>
+        <h3 class="box-title">Daftar Semua Pendaftar Program</h3>
       </div>
       <!-- /.box-header -->
       <div class="box-body">
         <table id="example1" class="table table-bordered table-striped">
           <thead>
           <tr>
-            <th>Rendering engine</th>
-            <th>Browser</th>
-            <th>Platform(s)</th>
-            <th>Engine version</th>
-            <th>CSS grade</th>
+            <th>Nama Lengkap</th>
+            <th>JK</th>
+            <th>Nama Ayah/Ibu</th>
+            <th>Alamat</th>
+            <th>Seleksi Program</th>
+            <th>Aksi</th>
           </tr>
           </thead>
           <tbody>
           <tr>
-            <td>Trident</td>
-            <td>Internet
-              Explorer 4.0
-            </td>
-            <td>Win 95+</td>
-            <td> 4</td>
-            <td>X</td>
+            <td><b>Dede Iskandar</b></td>
+            <td>L</td>
+            <td>Mahpudin / Iib</td>
+            <td>Sukabumi</td>
+            <td>BPB 3</td>
+            <td><button type="button" class="btn btn-info btn-xs">Edit</button> | <button type="button" class="btn btn-danger btn-xs">Hapus</button></td>
           </tr>
           <tr>
-            <td>Tasman</td>
-            <td>Internet Explorer 4.5</td>
-            <td>Mac OS 8-9</td>
-            <td>-</td>
-            <td>X</td>
-          </tr>
-          <tr>
-            <td>Tasman</td>
-            <td>Internet Explorer 5.1</td>
-            <td>Mac OS 7.6-9</td>
-            <td>1</td>
-            <td>C</td>
-          </tr>
-          <tr>
-            <td>Tasman</td>
-            <td>Internet Explorer 5.2</td>
-            <td>Mac OS 8-X</td>
-            <td>1</td>
-            <td>C</td>
-          </tr>
-          <tr>
-            <td><b><a href="/">Misc</a></b></td>
-            <td>NetFront 3.1</td>
-            <td>Embedded devices</td>
-            <td>-</td>
-            <td>C</td>
-          </tr>
-          <tr>
-            <td>Misc</td>
-            <td>NetFront 3.4</td>
-            <td>Embedded devices</td>
-            <td>-</td>
-            <td>A</td>
-          </tr>
-          <tr>
-            <td>Misc</td>
-            <td>Dillo 0.8</td>
-            <td>Embedded devices</td>
-            <td>-</td>
-            <td>X</td>
-          </tr>
-          <tr>
-            <td>Misc</td>
-            <td>Links</td>
-            <td>Text only</td>
-            <td>-</td>
-            <td>X</td>
-          </tr>
-          <tr>
-            <td>Misc</td>
-            <td>Lynx</td>
-            <td>Text only</td>
-            <td>-</td>
-            <td>X</td>
-          </tr>
-          <tr>
-            <td>Misc</td>
-            <td>IE Mobile</td>
-            <td>Windows Mobile 6</td>
-            <td>-</td>
-            <td>C</td>
-          </tr>
-          <tr>
-            <td>Misc</td>
-            <td>PSP browser</td>
-            <td>PSP</td>
-            <td>-</td>
-            <td>C</td>
-          </tr>
-          <tr>
-            <td>Other browsers</td>
-            <td>All others</td>
-            <td>-</td>
-            <td>-</td>
-            <td>U</td>
+            <td><b>Dede Iskandar</b></td>
+            <td>L</td>
+            <td>Mahpudin / Iib</td>
+            <td>Sukabumi</td>
+            <td>BPB 3</td>
+            <td><button type="button" class="btn btn-info btn-xs">Edit</button> | <button type="button" class="btn btn-danger btn-xs">Hapus</button></td>
           </tr>
           </tbody>
           <tfoot>
           <tr>
-            <th>Rendering engine</th>
-            <th>Browser</th>
-            <th>Platform(s)</th>
-            <th>Engine version</th>
-            <th>CSS grade</th>
+            <th>Nama Lengkap</th>
+            <th>Jenis Kelamin</th>
+            <th>Nama Orang tua</th>
+            <th>Alamat</th>
+            <th>Seleksi Program</th>
+            <th>Aksi</th>
           </tr>
           </tfoot>
         </table>
