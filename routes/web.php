4deb1ea81ab6000128e4b306ae45ab7f1f3ae3f4
kobeuu <kobeuu@gmail.com>
2017-07-07 18:20:36 +0700

    tambah halaman statis

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,17 @@
 |
 */
 
-Route::get('/', 'HomeController@index')->name('home')->middleware('auth');
+// Pages
+Route::get('/', 'PagesController@index')->name('home');
+Route::get('setting', 'PagesController@setting')->name('setting');
+Route::get('documentation', 'PagesController@documentation')->name('documentation');
+
+Auth::routes();
 
 // Authentication Routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+// Route::post('login', 'Auth\LoginController@login');
+// Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registrants Routes...
 Route::get('registrants', 'RegistrantController@index')->name('registrants');
